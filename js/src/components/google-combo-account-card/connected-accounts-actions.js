ec0b1c541023a4868ed2a4922f3c27ee748fce1a
Ankit Gade <ankit.gade@10up.com>
2024-10-28 21:00:41 +0530

    Remove ConversionMeasurementNotice component.

--- js/src/components/google-combo-account-card/connected-accounts-actions.js
+++ js/src/components/google-combo-account-card/connected-accounts-actions.js
@@ -1,8 +1,13 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
+import AppNotice from '.~/components/app-notice';
 import ClaimAdsAccount from './claim-ads-account/claim-ads-account';
-import ConversionMeasurementNotice from './conversion-measurement-notice';
 import useGoogleAdsAccount from '.~/hooks/useGoogleAdsAccount';
 import useGoogleAdsAccountStatus from '.~/hooks/useGoogleAdsAccountStatus';
 import useGoogleAdsAccountReady from '.~/hooks/useGoogleAdsAccountReady';
@@ -23,7 +28,18 @@ const ConnectedAccountsActions = () => {
 	return (
 		<div className="gla-connected-accounts-actions">
 			{ shouldClaimGoogleAdsAccount && <ClaimAdsAccount /> }
-			{ isReady && <ConversionMeasurementNotice /> }
+			{ isReady && (
+				<AppNotice
+					className="gla-ads-conversion-measurement-notice"
+					status="success"
+					isDismissible={ false }
+				>
+					{ __(
+						'Google Ads conversion measurement has been set up for your store.',
+						'google-listings-and-ads'
+					) }
+				</AppNotice>
+			) }
 		</div>
 	);
 };
