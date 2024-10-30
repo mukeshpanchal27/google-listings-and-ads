be9229b84a80178d68d4f6007a059f567b6eac15
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2024-10-29 19:12:33 -0600

    GTIN Migration controller

---
+++ src/API/Site/Controllers/GTINMigrationController.php
@@ -0,0 +1,88 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\API\TransportMethods;
+use Automattic\WooCommerce\GoogleListingsAndAds\Jobs\MigrateGTIN;
+use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\RESTServer;
+use Exception;
+use WP_REST_Request as Request;
+use WP_REST_Response as Response;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class GTINMigrationController offering API endpoint for GTIN field Migration
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers
+ */
+class GTINMigrationController extends BaseController {
+	use EmptySchemaPropertiesTrait;
+	/**
+	 * Job responsible to run the migration in the background.
+	 *
+	 * @var MigrateGTIN
+	 */
+	protected $job;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param RESTServer     $server
+	 * @param MigrateGTIN $job
+	 */
+	public function __construct( RESTServer $server, MigrateGTIN $job ) {
+		parent::__construct( $server );
+		$this->job = $job;
+	}
+
+	/**
+	 * Register rest routes with WordPress.
+	 */
+	public function register_routes(): void {
+		$this->register_route(
+			'gtin-migration-start',
+			[
+				[
+					'methods'             => TransportMethods::CREATABLE,
+					'callback'            => $this->start_migration_callback(),
+					'permission_callback' => $this->get_permission_callback(),
+					'args'                => $this->get_schema_properties(),
+				],
+				'schema' => $this->get_api_response_schema_callback(),
+			]
+		);
+	}
+
+
+	/**
+	 * Callback function for scheduling GTIN migration job.
+	 *
+	 * @return callable
+	 */
+	protected function start_migration_callback(): callable {
+		return function ( Request $request ) {
+			try {
+				if ( ! $this->job->can_schedule( [ 1 ] ) ) {
+					return new Response( [
+						'status'  => 'error',
+						'message' => __( 'GTIN Migration cannot be scheduled.', 'google-listings-and-ads' ),
+					], 400 );
+				}
+
+				$this->job->schedule();
+				return new Response( [
+					'status'  => 'success',
+					'message' => __( 'GTIN Migration successfully started.', 'google-listings-and-ads' ),
+				], 200 );
+			} catch ( Exception $e ) {
+				return $this->response_from_exception( $e );
+			}
+		};
+	}
+
+	protected function get_schema_title(): string {
+		return 'gtin_migration';
+	}
+}
