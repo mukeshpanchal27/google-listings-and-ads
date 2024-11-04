4e201c19b2a2679c1769efd7dc393b0eb06f1af2
asvinb <asvin.balloo@gmail.com>
2024-11-04 13:34:40 +0400

    Fix linting errors.

--- js/src/components/google-mc-account-card/index.js
+++ js/src/components/google-mc-account-card/index.js
@@ -1,1 +1,1 @@
-export { default as ConnectedGoogleMCAccountCard } from './connected-google-mc-account-card';
+export { default as MerchantCenterAccountInfoCard } from './merchant-center-account-info-card';
