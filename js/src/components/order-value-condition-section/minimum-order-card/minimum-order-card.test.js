3857f5a588b81b15de37abab65e6a0e9eae78054
Ian Yu-Hsun Lin <ianlin@automattic.com>
2024-11-22 23:00:35 +0800

    Add tests for free shipping order value condition

--- js/src/components/order-value-condition-section/minimum-order-card/minimum-order-card.test.js
+++ js/src/components/order-value-condition-section/minimum-order-card/minimum-order-card.test.js
@@ -9,89 +9,126 @@ import userEvent from '@testing-library/user-event';
  * Internal dependencies
  */
 import MinimumOrderCard from './minimum-order-card.js';
+import * as adaptiveForm from '.~/components/adaptive-form';
 
 jest.mock( '.~/hooks/useAppSelectDispatch' );
 jest.mock( '.~/hooks/useCountryKeyNameMap' );
 jest.mock( '.~/hooks/useStoreCurrency' );
+jest.mock( '.~/components/adaptive-form', () => {
+	return {
+		__esModule: true,
+		...jest.requireActual( '.~/components/adaptive-form' ),
+	};
+} );
 
-jest.mock( '.~/components/adaptive-form', () => ( {
-	useAdaptiveFormContext: jest
-		.fn()
-		.mockName( 'useAdaptiveFormContext' )
-		.mockImplementation( () => {
-			return {
-				getInputProps: () => {
-					return {
-						checked: true,
-						className: '',
-						help: null,
-						onBlur: () => {},
-						onChange: () => {},
-						selected: 'flat',
-						value: 'flat',
-					};
-				},
-				values: {
-					countries: [ 'ES' ],
-					language: 'English',
-					locale: 'en_US',
-					location: 'selected',
-					offer_free_shipping: false,
-					shipping_country_rates: [],
-					shipping_country_times: [],
-					shipping_rate: 'flat',
-					shipping_time: 'flat',
-					tax_rate: null,
-				},
-			};
-		} ),
-	useAdaptiveFormInputProps: jest
-		.fn()
-		.mockName( 'useAdaptiveFormInputProps' )
-		.mockImplementation( () => {
-			return {
-				checked: true,
-				className: '',
-				help: null,
-				onBlur: () => {},
-				onChange: () => {},
-				selected: 'flat',
-				value: 'flat',
-			};
-		} ),
-} ) );
+const mockGetInputProps = jest.fn().mockImplementation( () => {
+	return {
+		checked: true,
+		className: '',
+		help: null,
+		onBlur: () => {},
+		onChange: () => {},
+		selected: true,
+		value: true,
+	};
+} );
+
+const adaptiveFormContextDefaultValues = {
+	countries: [ 'ES' ],
+	language: 'English',
+	locale: 'en_US',
+	location: 'selected',
+	offer_free_shipping: true,
+	shipping_country_rates: [],
+	shipping_country_times: [],
+	shipping_rate: true,
+	shipping_time: true,
+	tax_rate: null,
+};
+
+const adaptiveFormInputPropsDefaultValues = {
+	checked: true,
+	className: '',
+	help: null,
+	onBlur: () => {},
+	onChange: () => {},
+	selected: true,
+	value: true,
+};
+
+const getMinimumOrderCardDefaultValue = () => {
+	return Object.freeze( [
+		{
+			id: '1',
+			country: 'US',
+			currency: 'USD',
+			rate: 20,
+			options: {},
+		},
+		{
+			id: '2',
+			country: 'ES',
+			currency: 'USD',
+			rate: 20,
+			options: {
+				free_shipping_threshold: 50,
+			},
+		},
+		{
+			id: '3',
+			country: 'CN',
+			currency: 'USD',
+			rate: 25,
+			options: {
+				free_shipping_threshold: 50,
+			},
+		},
+	] );
+};
+
+const spyAdaptiveFormFunction = ( functionName ) => {
+	return jest.spyOn( adaptiveForm, functionName );
+};
+
+const mockAdaptiveFormContextImplementation = ( spy, values = {} ) => {
+	spy.mockImplementation( () => ( {
+		getInputProps: mockGetInputProps(),
+		values: {
+			...adaptiveFormContextDefaultValues,
+			...values,
+		},
+	} ) );
+};
+
+const mockAdaptiveFormInputPropsImplementation = ( spy, values = {} ) => {
+	spy.mockImplementation( () => ( {
+		...adaptiveFormInputPropsDefaultValues,
+		...values,
+	} ) );
+};
 
 describe( 'MinimumOrderCard', () => {
+	let value,
+		onChange,
+		rendered,
+		spyAdaptiveFormContext,
+		spyAdaptiveFormInputProps;
+
 	describe( 'onChange callback property', () => {
-		let value, onChange, rendered;
 		beforeEach( () => {
-			value = Object.freeze( [
-				{
-					id: '1',
-					country: 'US',
-					currency: 'USD',
-					rate: 20,
-					options: {},
-				},
-				{
-					id: '2',
-					country: 'ES',
-					currency: 'USD',
-					rate: 20,
-					options: {
-						free_shipping_threshold: 50,
-					},
-				},
-				{
-					id: '3',
-					country: 'CN',
-					currency: 'USD',
-					rate: 25,
-					options: {
-						free_shipping_threshold: 50,
-					},
-				},
-			] );
+			spyAdaptiveFormContext = spyAdaptiveFormFunction(
+				'useAdaptiveFormContext'
+			);
+			mockAdaptiveFormContextImplementation( spyAdaptiveFormContext );
+
+			spyAdaptiveFormInputProps = spyAdaptiveFormFunction(
+				'useAdaptiveFormInputProps'
+			);
+			mockAdaptiveFormInputPropsImplementation(
+				spyAdaptiveFormInputProps
+			);
+
+			value = getMinimumOrderCardDefaultValue();
 			onChange = jest.fn().mockName( 'onChange callback' );
 
 			rendered = render(
@@ -233,6 +270,73 @@ describe( 'MinimumOrderCard', () => {
 			expectedValue[ 1 ].options.free_shipping_threshold = undefined;
 			expectedValue[ 2 ].options.free_shipping_threshold = undefined;
 			expect( onChange ).toHaveBeenCalledWith( expectedValue );
+		} );
+	} );
+
+	describe( 'Free shipping order value condition', () => {
+		beforeEach( () => {
+			value = getMinimumOrderCardDefaultValue();
+			onChange = jest.fn().mockName( 'onChange callback' );
+		} );
+
+		afterEach( () => {
+			jest.clearAllMocks();
+		} );
+
+		test( 'When no free shipping is offered, hides the input field', async () => {
+			spyAdaptiveFormContext = spyAdaptiveFormFunction(
+				'useAdaptiveFormContext'
+			);
+			mockAdaptiveFormContextImplementation( spyAdaptiveFormContext, {
+				offer_free_shipping: false,
+			} );
+
+			spyAdaptiveFormInputProps = spyAdaptiveFormFunction(
+				'useAdaptiveFormInputProps'
+			);
+			mockAdaptiveFormInputPropsImplementation(
+				spyAdaptiveFormInputProps
+			);
+
+			rendered = render(
+				<MinimumOrderCard value={ value } onChange={ onChange } />
+			);
+
+			const { container } = rendered;
+
+			const inputControl = container.querySelector(
+				'.gla-minimum-order-input-control'
+			);
+
+			expect( inputControl ).toHaveClass( 'hide_input' );
+		} );
+
+		test( 'When free shipping is offered, do not hide the input field', async () => {
+			spyAdaptiveFormContext = spyAdaptiveFormFunction(
+				'useAdaptiveFormContext'
+			);
+			mockAdaptiveFormContextImplementation( spyAdaptiveFormContext, {
+				offer_free_shipping: true,
+			} );
+
+			spyAdaptiveFormInputProps = spyAdaptiveFormFunction(
+				'useAdaptiveFormInputProps'
+			);
+			mockAdaptiveFormInputPropsImplementation(
+				spyAdaptiveFormInputProps
+			);
+
+			rendered = render(
+				<MinimumOrderCard value={ value } onChange={ onChange } />
+			);
+
+			const { container } = rendered;
+
+			const inputControl = container.querySelector(
+				'.gla-minimum-order-input-control'
+			);
+
+			expect( inputControl ).not.toHaveClass( 'hide_input' );
 		} );
 	} );
 } );
