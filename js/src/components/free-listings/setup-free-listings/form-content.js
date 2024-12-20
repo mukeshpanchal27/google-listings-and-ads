164c233969dba48ebba32d7185d49f7c5f113a99
Eason Su <eason.su.tw@gmail.com>
2024-12-20 17:50:04 +0800

    Move the `TaxRate` component to the `pages/settings/setup-tax-rate` directory.

--- js/src/components/free-listings/setup-free-listings/form-content.js
+++ js/src/components/free-listings/setup-free-listings/form-content.js
@@ -10,7 +10,7 @@ import { useAdaptiveFormContext } from '~/components/adaptive-form';
 import StepContent from '~/components/stepper/step-content';
 import StepContentActions from '~/components/stepper/step-content-actions';
 import StepContentFooter from '~/components/stepper/step-content-footer';
-import TaxRate from '~/components/free-listings/configure-product-listings/tax-rate';
+import TaxRate from '~/pages/settings/setup-tax-rate/tax-rate';
 import useDisplayTaxRate from '~/pages/settings/setup-tax-rate/useDisplayTaxRate';
 import ChooseAudienceSection from '~/components/free-listings/choose-audience-section';
 import ShippingRateSection from '~/components/shipping-rate-section';
