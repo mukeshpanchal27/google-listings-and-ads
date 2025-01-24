fa3185d368ad02859fcaba9f65c5292aa5b66849
Eason Su <eason.su.tw@gmail.com>
2025-01-24 17:12:35 +0800

    Implement a new component `WPComAppAuthorizationCard`.

--- js/src/components/wpcom-account-card/index.js
+++ js/src/components/wpcom-account-card/index.js
@@ -1,3 +1,4 @@
 export { default } from './wpcom-account-card';
 export { default as ConnectWPComAccountCard } from './connect-wpcom-account-card';
 export { default as ConnectedWPComAccountCard } from './connected-wpcom-account-card';
+export { default as AuthorizeWPComAppCard } from './authorize-wpcom-app-card';
