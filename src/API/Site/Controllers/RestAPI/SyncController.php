534889aef19ca95e5576ec6878081adb85212b5b
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2025-02-18 11:46:05 +0300

    Add post method for Sync Endpoint

--- src/API/Site/Controllers/RestAPI/SyncController.php
+++ src/API/Site/Controllers/RestAPI/SyncController.php
@@ -68,6 +68,12 @@ class SyncController extends BaseOptionsController {
 					'callback'            => $this->get_sync_callback(),
 					'permission_callback' => $this->get_permission_callback(),
 				],
+				[
+					'methods'             => TransportMethods::EDITABLE,
+					'callback'            => $this->get_update_sync_callback(),
+					'permission_callback' => $this->get_permission_callback(),
+					'args'                => $this->get_update_sync_params(),
+				],
 				'schema' => $this->get_api_response_schema_callback(),
 			]
 		);
@@ -81,13 +87,27 @@ class SyncController extends BaseOptionsController {
 	protected function get_sync_callback(): callable {
 		return function ( Request $request ) {
 			try {
-				$sync_mode = $this->options->get( OptionsInterface::API_PULL_SYNC_MODE );
-				if ( ! is_array( $sync_mode ) ) {
-					$sync_mode = [];
-				}
-
-				$sync_mode = wp_parse_args( $sync_mode, self::DEFAULT_SYNC_MODE );
+				$sync_mode = $this->get_current_sync_value();
 				return $this->prepare_item_for_response( $sync_mode, $request );
+			} catch ( Exception $e ) {
+				return $this->response_from_exception( $e );
+			}
+		};
+	}
+
+	/**
+	 * Get the callback function for updating the current sync mode for API PULL.
+	 *
+	 * @return callable
+	 */
+	protected function get_update_sync_callback(): callable {
+		return function ( Request $request ) {
+			try {
+				$sync_mode     = $this->get_current_sync_value();
+				$new_params    = $this->get_request_params( $request );
+				$new_sync_mode = wp_parse_args( $new_params, $sync_mode );
+				$this->options->update( OptionsInterface::API_PULL_SYNC_MODE, $new_sync_mode );
+				return $this->prepare_item_for_response( $this->options->get( OptionsInterface::API_PULL_SYNC_MODE ), $request );
 			} catch ( Exception $e ) {
 				return $this->response_from_exception( $e );
 			}
@@ -113,19 +133,19 @@ class SyncController extends BaseOptionsController {
 	protected function get_schema_properties(): array {
 		return [
 			'products' => [
-				'type'  => 'array',
+				'type'  => 'object',
 				'items' => $this->get_pull_push_schema_fields(),
 			],
 			'coupons'  => [
-				'type'  => 'array',
+				'type'  => 'object',
 				'items' => $this->get_pull_push_schema_fields(),
 			],
 			'shipping' => [
-				'type'  => 'array',
+				'type'  => 'object',
 				'items' => $this->get_pull_push_schema_fields(),
 			],
 			'settings' => [
-				'type'  => 'array',
+				'type'  => 'object',
 				'items' => $this->get_pull_push_schema_fields(),
 			],
 		];
@@ -136,7 +156,7 @@ class SyncController extends BaseOptionsController {
 	 *
 	 * @return array[]
 	 */
-	private function get_pull_push_schema_fields() {
+	private function get_pull_push_schema_fields(): array {
 		return [
 			'push' => [
 				'type' => 'boolean',
@@ -145,5 +165,52 @@ class SyncController extends BaseOptionsController {
 				'type' => 'boolean',
 			],
 		];
+	}
+
+	/**
+	 * Get the query params for the update sync request.
+	 *
+	 * @return array
+	 */
+	protected function get_update_sync_params(): array {
+		return $this->get_schema_properties();
+	}
+
+	/**
+	 * Get the current value for the API PULL Sync
+	 *
+	 * @return array
+	 */
+	protected function get_current_sync_value(): array {
+		$sync_mode = $this->options->get( OptionsInterface::API_PULL_SYNC_MODE );
+		if ( ! is_array( $sync_mode ) ) {
+			$sync_mode = [];
+		}
+
+		return wp_parse_args( $sync_mode, self::DEFAULT_SYNC_MODE );
+	}
+
+	/**
+	 * Get the parameters from the request body.
+	 * Only the keys in self::DEFAULT_SYNC_MODE that contains pull/push values as boolean are allowed as params.
+	 *
+	 * @param Request $request The request
+	 * @return array
+	 */
+	protected function get_request_params( Request $request ): array {
+		$request_params = $request->get_json_params();
+		$params         = array_intersect_key( $request_params, self::DEFAULT_SYNC_MODE );
+		$valid_params   = [];
+
+		foreach ( $params as $key => $param ) {
+			if ( isset( $param['push'] ) && isset( $param['pull'] ) && is_bool( $param['push'] ) && is_bool( $param['pull'] ) ) {
+				$valid_params[ $key ] = [
+					'push' => $param['push'],
+					'pull' => $param['pull'],
+				];
+			}
+		}
+
+		return $valid_params;
 	}
 }
