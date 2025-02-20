3704481c4e31ea0aac49cad2572f0eca557a5a99
Eason Su <eason.su.tw@gmail.com>
2025-02-20 18:32:10 +0800

    In onboarding flow, add a process to seamlessly and automatically redirect to WPCOM app authorization.

--- js/src/pages/onboarding/index.js
+++ js/src/pages/onboarding/index.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import useLayout from '~/hooks/useLayout';
+import useAutoWPComAppAuthorization from './useAutoWPComAppAuthorization';
 import useUpdateRestAPIAuthorizeStatusByUrlQuery from '~/hooks/useUpdateRestAPIAuthorizeStatusByUrlQuery';
 import SetupTopBar from './setup-top-bar';
 import SetupStepper from './setup-stepper';
@@ -14,6 +15,14 @@ import SetupStepper from './setup-stepper';
 const Onboarding = () => {
 	useLayout( 'full-page' );
 	useUpdateRestAPIAuthorizeStatusByUrlQuery();
+
+	const canContinueRendering = useAutoWPComAppAuthorization();
+
+	// Render nothing as the requirement is to make it look like the redirections
+	// between Google authorization and WPCOM app authorization are seamless.
+	if ( ! canContinueRendering ) {
+		return null;
+	}
 
 	return (
 		<>
