aebf4446f201e3bfbfc3ab7ec59bf638f5af25ed
Eason Su <eason.su.tw@gmail.com>
2025-03-13 15:20:07 +0800

    Record an event tracking when the user is automatically redirected to WPCom app authorization.

--- js/src/pages/onboarding/index.js
+++ js/src/pages/onboarding/index.js
@@ -9,6 +9,7 @@ import { Spinner } from '@woocommerce/components';
 import useLayout from '~/hooks/useLayout';
 import useAutoWPComAppAuthorization from './useAutoWPComAppAuthorization';
 import useUpdateRestAPIAuthorizeStatusByUrlQuery from '~/hooks/useUpdateRestAPIAuthorizeStatusByUrlQuery';
+import { CONTEXT_EXTENSION_ONBOARDING } from '~/utils/tracks';
 import SetupTopBar from './setup-top-bar';
 import SetupStepper from './setup-stepper';
 
@@ -21,7 +22,9 @@ const Onboarding = () => {
 	useLayout( 'full-page' );
 	useUpdateRestAPIAuthorizeStatusByUrlQuery();
 
-	const canContinueRendering = useAutoWPComAppAuthorization();
+	const canContinueRendering = useAutoWPComAppAuthorization(
+		CONTEXT_EXTENSION_ONBOARDING
+	);
 
 	// Render a spinner only as the requirement is to make it look like the redirections
 	// between Google authorization and WPCOM app authorization are seamless.
