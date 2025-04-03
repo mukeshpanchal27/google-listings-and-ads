3ce978f3cd0a6d3b30f62157398dcdacf3fc4313
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-04-03 17:14:39 +0530

    Add rest API endpoint

---
+++ src/API/Site/Controllers/MerchantCenter/PriceBenchmarksController.php
@@ -0,0 +1,121 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\MerchantCenter;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\BaseController;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\TransportMethods;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query\MerchantPriceBenchmarksQuery;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query\MerchantPriceSuggestionsQuery;
+use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsAwareTrait;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class PriceBenchmarksController
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\MerchantCenter
+ */
+class PriceBenchmarksController extends BaseController {
+
+	use OptionsAwareTrait;
+
+	/**
+	 * Register rest routes with WordPress.
+	 */
+	public function register_routes(): void {
+		$this->register_route(
+			'mc/price-benchmarks',
+			[
+				[
+					'methods'             => TransportMethods::READABLE,
+					'callback'            => $this->get_price_benchmarks_callback(),
+					//'permission_callback' => $this->get_permission_callback(),
+				],
+				'schema' => $this->get_api_response_schema_callback(),
+			]
+		);
+	}
+
+	/**
+	 * Get the callback function for the price benchmarks request.
+	 *
+	 * @return callable
+	 */
+	protected function get_price_benchmarks_callback(): callable {
+		return function () {
+			try {
+				$benchmarks_query = new MerchantPriceBenchmarksQuery(
+					[
+						'next_page' => 2,
+						'per_page'  => 10,
+					]
+				);
+				$response = $benchmarks_query
+					->set_client( $this->service, $merchant_id )
+					->get_results();
+
+				return rest_ensure_response( $response );
+			} catch ( \Exception $e ) {
+				return new \WP_Error(
+					'price_benchmarks_error',
+					__( 'Failed to fetch price benchmarks.', 'google-listings-and-ads' ),
+					[
+						'status'  => 500,
+						'message' => $e->getMessage(),
+					]
+				);
+			}
+		};
+	}
+
+	/**
+	 * Get the schema for settings endpoints.
+	 *
+	 * @return array
+	 */
+	protected function get_schema_properties(): array {
+		return [
+			'product' => [
+				'description' => __( 'Product details.', 'google-listings-and-ads' ),
+				'type'        => 'object',
+				'properties'  => [
+					'id'        => [ 'type' => 'integer' ],
+					'thumbnail' => [ 'type' => 'string', 'format' => 'uri' ],
+					'title'     => [ 'type' => 'string' ],
+				],
+			],
+			'effectiveness' => [
+				'description' => __( 'Effectiveness score.', 'google-listings-and-ads' ),
+				'type'        => 'number',
+			],
+			'regular_price' => [
+				'description' => __( 'Regular price of the product.', 'google-listings-and-ads' ),
+				'type'        => 'number',
+			],
+			'price_on_google' => [
+				'description' => __( 'Price of the product on Google.', 'google-listings-and-ads' ),
+				'type'        => 'number',
+			],
+			'price_gap' => [
+				'description' => __( 'Price gap between the regular price and the price on Google.', 'google-listings-and-ads' ),
+				'type'        => 'number',
+			],
+			'suggested_price' => [
+				'description' => __( 'Suggested price for the product.', 'google-listings-and-ads' ),
+				'type'        => 'number',
+			],
+		];
+	}
+
+	/**
+	 * Get the item schema name for the controller.
+	 *
+	 * Used for building the API response schema.
+	 *
+	 * @return string
+	 */
+	protected function get_schema_title(): string {
+		return 'price_benchmarks';
+	}
+}
