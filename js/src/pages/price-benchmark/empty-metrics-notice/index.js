09e7fba4003a4440e4f84e7f5765cbdf6a0767c4
asvinb <asvin.balloo@gmail.com>
2025-04-29 12:39:53 +0400

    Do not use EmptyTable component.

--- js/src/pages/price-benchmark/empty-metrics-notice/index.js
+++ js/src/pages/price-benchmark/empty-metrics-notice/index.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { EmptyTable } from '@woocommerce/components';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
@@ -15,47 +14,43 @@ import './index.scss';
 const EmptyMetricsNotice = () => {
 	return (
 		<div className="gla-price-benchmark__empty-metrics">
-			<EmptyTable>
-				<div className="gla-price-benchmark__empty-metrics-columns">
-					<div className="gla-price-benchmark__empty-metrics-graphic">
-						<img
-							src={ EmptyMetricsGraphic }
-							alt={ __(
-								'No sale price suggestions graphic',
-								'google-listings-and-ads'
-							) }
-							width={ 88 }
-							height={ 88 }
-						/>
-					</div>
-					<div className="gla-price-benchmark__empty-metrics-description">
-						<p>
-							{ __(
-								'You do not have any sale price suggestions at this moment.',
-								'google-listings-and-ads'
-							) }
-						</p>
-						<p>
-							{ createInterpolateElement(
-								__(
-									'<a>Find out</a> if you meet all eligibility criteria to receive suggestions in the future.',
-									'google-listings-and-ads'
-								),
-								{
-									a: (
-										<AppDocumentationLink
-											className="gla-empty-metrics-notice__link"
-											href="https://support.google.com/merchants/answer/13798101"
-											context="price-benchmark-empty-metrics-notice"
-											linkId="empty-metric-notice-find-out-link"
-										/>
-									),
-								}
-							) }
-						</p>
-					</div>
-				</div>
-			</EmptyTable>
+			<div className="gla-price-benchmark__empty-metrics-graphic">
+				<img
+					src={ EmptyMetricsGraphic }
+					alt={ __(
+						'No sale price suggestions graphic',
+						'google-listings-and-ads'
+					) }
+					width={ 88 }
+					height={ 88 }
+				/>
+			</div>
+			<div className="gla-price-benchmark__empty-metrics-description">
+				<p>
+					{ __(
+						'You do not have any sale price suggestions at this moment.',
+						'google-listings-and-ads'
+					) }
+				</p>
+				<p>
+					{ createInterpolateElement(
+						__(
+							'<a>Find out</a> if you meet all eligibility criteria to receive suggestions in the future.',
+							'google-listings-and-ads'
+						),
+						{
+							a: (
+								<AppDocumentationLink
+									className="gla-empty-metrics-notice__link"
+									href="https://support.google.com/merchants/answer/13798101"
+									context="price-benchmark-empty-metrics-notice"
+									linkId="empty-metric-notice-find-out-link"
+								/>
+							),
+						}
+					) }
+				</p>
+			</div>
 		</div>
 	);
 };
