e342b19aa901e0d2f0f447a215adefc08a6ab51b
Ankit Gade <ankit.gade@10up.com>
2025-04-30 19:39:23 +0530

    Simplify condition.

--- js/src/wp-dataviews.js
+++ js/src/wp-dataviews.js
@@ -3,8 +3,5 @@
  */
 import * as dataviews from '../../node_modules/@wordpress/dataviews/build-wp';
 
-if ( typeof window.wp === 'undefined' ) {
-	window.wp = {};
-}
-
+window.wp = window.wp || {};
 window.wp.dataviews = dataviews;
