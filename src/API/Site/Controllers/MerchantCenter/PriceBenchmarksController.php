e57b659a64b975cb98a87ccce7659eb4b7f4621d
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-05-02 14:19:46 +0530

    Initial implementation

--- src/API/Site/Controllers/MerchantCenter/PriceBenchmarksController.php
+++ src/API/Site/Controllers/MerchantCenter/PriceBenchmarksController.php
@@ -5,8 +5,10 @@ namespace Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\Merch
 
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\BaseController;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\TransportMethods;
-use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query\MerchantPriceSuggestionsQuery;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\MerchantPriceBenchmarks;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\MerchantReport;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query\MerchantPriceSuggestionsQuery;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query\ReportQueryTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\Internal\ContainerAwareTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\Internal\Interfaces\ContainerAwareInterface;
 use Exception;
@@ -23,6 +25,7 @@ defined( 'ABSPATH' ) || exit;
 class PriceBenchmarksController extends BaseController implements ContainerAwareInterface {
 
 	use ContainerAwareTrait;
+	use ReportQueryTrait;
 
 	/**
 	 * Register rest routes with WordPress.
@@ -35,6 +38,7 @@ class PriceBenchmarksController extends BaseController implements ContainerAware
 					'methods'             => TransportMethods::READABLE,
 					'callback'            => $this->get_price_benchmarks_callback(),
 					'permission_callback' => $this->get_permission_callback(),
+					'args'                => $this->get_price_benchmarks_params(),
 				],
 				'schema' => $this->get_api_response_schema_callback(),
 			]
@@ -70,12 +74,13 @@ class PriceBenchmarksController extends BaseController implements ContainerAware
 				/** @var MerchantPriceBenchmarks $merchant */
 				$merchant = $this->container->get( MerchantPriceBenchmarks::class );
 
+				$merchant_report_data = $this->get_products_report_data( $request );
+
 				$benchmark_data      = $merchant->get_benchmark_data( $this->prepare_query_arguments( $request ) );
 				$price_insights_data = $merchant->get_price_insights( $this->prepare_query_arguments( $request ) );
 
 				// Map the data to the required format.
-				$response_data = $this->map_price_benchmarks_response( $benchmark_data, $price_insights_data );
-
+				$response_data = $this->map_price_benchmarks_response( $benchmark_data, $price_insights_data, $merchant_report_data );
 				return new Response( $response_data );
 			} catch ( Exception $e ) {
 				return $this->response_from_exception( $e );
@@ -84,13 +89,48 @@ class PriceBenchmarksController extends BaseController implements ContainerAware
 	}
 
 	/**
+	 * Retrieves the products report data for the given request.
+	 *
+	 * @param Request $request REST Request.
+	 * @return array|WP_Error Prepared response data or error.
+	 */
+	protected function get_products_report_data( Request $request ): array {
+		try {
+			/** @var MerchantReport $merchant_report */
+			$merchant_report         = $this->container->get( MerchantReport::class );
+			$prepare_query_arguments = $this->prepare_query_arguments( $request );
+			$product_id              = $request->get_param( 'id' );
+
+			// If a product ID is provided, set it in the query arguments.
+			if ( $product_id ) {
+				$prepare_query_arguments['ids']      = [ $product_id ];
+				$prepare_query_arguments['fields']   = [ 'clicks', 'conversions' ];
+				$prepare_query_arguments['interval'] = 'week';
+			}
+
+			$report_data = $merchant_report->get_report_data( 'products', $prepare_query_arguments );
+
+			// Ensure the response is always an array.
+			if ( is_wp_error( $report_data ) ) {
+				return [];
+			}
+
+			return $report_data;
+		} catch ( Exception $exception ) {
+			// Log the exception and return an empty array.
+			return [];
+		}
+	}
+
+	/**
 	 * Maps the benchmark and price insights data to the required API response format.
 	 *
-	 * @param array $benchmark_data Raw benchmark data.
-	 * @param array $price_insights_data Raw price insights data.
+	 * @param array $benchmark_data       Raw benchmark data.
+	 * @param array $price_insights_data  Raw price insights data.
+	 * @param array $merchant_report_data Raw merchant report data.
 	 * @return array Mapped response data.
 	 */
-	protected function map_price_benchmarks_response( array $benchmark_data, array $price_insights_data ): array {
+	protected function map_price_benchmarks_response( array $benchmark_data, array $price_insights_data, array $merchant_report_data ): array {
 		$mapped_data = [];
 
 		if ( empty( $benchmark_data['results'] ) || empty( $price_insights_data['results'] ) ) {
@@ -145,6 +185,12 @@ class PriceBenchmarksController extends BaseController implements ContainerAware
 					'suggested_price' => isset( $price_insights['suggested_price_micros'] )
 						? round( $price_insights['suggested_price_micros'] / 1000000, 2 )
 						: '',
+					...( ! empty( $merchant_report_data ) ? [
+						'clicks'                       => 30,
+						'conversions'                  => 3,
+						'predicted_clicks_change'      => $price_insights['predicted_clicks_change_fraction'] ?? null,
+						'predicted_conversions_change' => $price_insights['predicted_conversions_change_fraction'] ?? null,
+					] : [] ),
 				];
 			},
 			$mapped_data
@@ -178,7 +224,7 @@ class PriceBenchmarksController extends BaseController implements ContainerAware
 	 */
 	protected function get_schema_properties(): array {
 		return [
-			'product'         => [
+			'product'                      => [
 				'description' => __( 'Product details.', 'google-listings-and-ads' ),
 				'type'        => 'object',
 				'properties'  => [
@@ -190,25 +236,78 @@ class PriceBenchmarksController extends BaseController implements ContainerAware
 					'title'     => [ 'type' => 'string' ],
 				],
 			],
-			'effectiveness'   => [
+			'effectiveness'                => [
 				'description' => __( 'Effectiveness score.', 'google-listings-and-ads' ),
 				'type'        => 'number',
 			],
-			'regular_price'   => [
+			'regular_price'                => [
 				'description' => __( 'Regular price of the product.', 'google-listings-and-ads' ),
 				'type'        => 'number',
 			],
-			'price_on_google' => [
+			'price_on_google'              => [
 				'description' => __( 'Price of the product on Google.', 'google-listings-and-ads' ),
 				'type'        => 'number',
 			],
-			'price_gap'       => [
+			'price_gap'                    => [
 				'description' => __( 'Price gap between the regular price and the price on Google.', 'google-listings-and-ads' ),
 				'type'        => 'number',
 			],
-			'suggested_price' => [
+			'suggested_price'              => [
 				'description' => __( 'Suggested price for the product.', 'google-listings-and-ads' ),
 				'type'        => 'number',
+			],
+			'clicks'                       => [
+				'description' => __( 'Current clicks.', 'google-listings-and-ads' ),
+				'type'        => [ 'integer', 'null' ],
+			],
+			'conversions'                  => [
+				'description' => __( 'Current conversions.', 'google-listings-and-ads' ),
+				'type'        => [ 'integer', 'null' ],
+			],
+			'predicted_clicks_change'      => [
+				'description' => __( 'Expected uplift in clicks (fraction).', 'google-listings-and-ads' ),
+				'type'        => [ 'string', 'null' ],
+			],
+			'predicted_conversions_change' => [
+				'description' => __( 'Expected uplift in conversions (fraction).', 'google-listings-and-ads' ),
+				'type'        => [ 'string', 'null' ],
+			],
+		];
+	}
+
+	/**
+	 * Retrieves the parameters for price benchmarks.
+	 *
+	 * @return array An associative array of parameters for price benchmarks.
+	 */
+	public function get_price_benchmarks_params(): array {
+		return [
+			'ids'      => [
+				'description'       => __( 'Limit result to items with specified ids.', 'google-listings-and-ads' ),
+				'type'              => 'array',
+				'sanitize_callback' => 'wp_parse_slug_list',
+				'validate_callback' => 'rest_validate_request_arg',
+				'items'             => [
+					'type' => 'string',
+				],
+			],
+			'fields'   => [
+				'description' => __( 'Comma-separated list of fields to include in the response.', 'google-listings-and-ads' ),
+				'type'        => 'array',
+				'required'    => false,
+			],
+			'interval' => [
+				'description'       => __( 'Time interval to use for segments in the returned data.', 'google-listings-and-ads' ),
+				'type'              => 'string',
+				'required'          => false,
+				'enum'              => [
+					'day',
+					'week',
+					'month',
+					'quarter',
+					'year',
+				],
+				'validate_callback' => 'rest_validate_request_arg',
 			],
 		];
 	}
