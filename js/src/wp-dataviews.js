0326978c939294e9f64e63334e7ea234d4e6d4b3
Ankit Gade <ankit.gade@10up.com>
2025-05-06 19:08:22 +0530

    Import only necessary functions.

--- js/src/wp-dataviews.js
+++ js/src/wp-dataviews.js
@@ -1,7 +1,12 @@
 /**
  * Internal dependencies
  */
-import * as dataviews from '../../node_modules/@wordpress/dataviews/build-wp';
+import {
+	DataViews,
+	filterSortAndPaginate,
+} from '../../node_modules/@wordpress/dataviews/build-wp';
 
 window.wp = window.wp || {};
-window.wp.dataviews = dataviews;
+window.wp.dataviews = {};
+window.wp.dataviews.DataViews = DataViews;
+window.wp.dataviews.filterSortAndPaginate = filterSortAndPaginate;
