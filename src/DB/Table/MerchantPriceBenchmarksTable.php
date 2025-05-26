c1858947925d94824aee131ea38f12a0e898c7e4
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-05-26 16:38:08 +0530

    Update DB schema

--- src/DB/Table/MerchantPriceBenchmarksTable.php
+++ src/DB/Table/MerchantPriceBenchmarksTable.php
@@ -24,17 +24,28 @@ class MerchantPriceBenchmarksTable extends Table {
 	protected function get_install_query(): string {
 		return "
 CREATE TABLE `{$this->get_sql_safe_name()}` (
-	product_id varchar(255) NOT NULL,
-	id varchar(255) NOT NULL,
-	price_micros bigint(20) NOT NULL,
-	currency_code varchar(3) NOT NULL,
-	country_code varchar(2) NOT NULL,
-	benchmark_price_micros bigint(20) NOT NULL,
-	benchmark_price_currency_code varchar(3) NOT NULL,
+	product_id bigint(20) NOT NULL,
+	mc_product_id varchar(255) NOT NULL,
+	mc_product_offer_id varchar(255) NOT NULL,
+	mc_product_price_micros varchar(64) NOT NULL,
+	mc_product_currency_code varchar(3) NOT NULL,
+	mc_price_country_code varchar(2) NOT NULL,
+	mc_price_benchmark_price_micros varchar(64) NOT NULL,
+	mc_price_benchmark_price_currency_code varchar(3) NOT NULL,
+	mc_insights_suggested_price_micros varchar(64) NOT NULL,
+	mc_insights_suggested_price_currency_code varchar(3) NOT NULL,
+	mc_insights_predicted_impressions_change_fraction decimal(10,6) NOT NULL,
+	mc_insights_predicted_clicks_change_fraction decimal(10,6) NOT NULL,
+	mc_insights_predicted_conversions_change_fraction decimal(10,6) NOT NULL,
+	mc_insights_effectiveness tinyint(1) NOT NULL,
+	mc_metrics_clicks varchar(64) NOT NULL,
+	mc_metrics_impressions varchar(64) NOT NULL,
+	mc_metrics_ctr int(20) NOT NULL,
+	mc_metrics_conversions int(20) NOT NULL,
 	price_compared_with_benchmark tinyint(1) NOT NULL DEFAULT 0,
-	PRIMARY KEY (product_id, id),
-	KEY currency_code (currency_code),
-	KEY country_code (country_code),
+	PRIMARY KEY (product_id),
+	UNIQUE KEY mc_product_id (mc_product_id)
+	KEY mc_insights_effectiveness (mc_insights_effectiveness)
 	KEY price_compared_with_benchmark (price_compared_with_benchmark)
 ) {$this->get_collation()};
 ";
